dd2e6a7eacaee7e3c994a8f68a4927dcf3c2eac9
Olivier-Kango <olivierkango@gmail.com>
2023-01-05 20:54:51 -0800

    Solve the error

--- app.rb
+++ app.rb
@@ -15,37 +15,36 @@ class App
       '6': 'List all rentals for a given person id',
       '7': 'Exit'
     }
-    # List all books.
+
+    # List of books
     @books = []
-    # List all people.
-    @peoples = []
+
+    # List of people
+    @people = []
   end
 
   # display commands
   def display_command
     @command.each do |index, command|
-      puts "#{index} -#{command}"
+      puts "#{index} - #{command}"
     end
   end
 
   # Print out all books
   def list_all_books
     if @books.empty?
-      puts 'Please insert books first!'
+      puts 'Please insert books first!!'
     else
       @books.each_with_index do |book, index|
-        puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+        puts "#{index}) Title: #{book.title}, Author: #{book.author}}"
       end
-    end
-    @books.each do |book|
-      puts book
     end
   end
 
-  # Print out all people
+  # Print out all books
   def list_all_people
     if @people.empty?
-      puts 'Please insert people first !!'
+      puts 'Please insert people first!!'
     else
       @people.each do |person|
         puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
@@ -74,28 +73,28 @@ class App
     when 'n' then parent_permission = false
     end
 
-    # Create a student and push him to people array
-    @people.push(Student.new('12', age, name, parent_permission: parent_permission))
+    # Create a student and push it to people array
+    @people.push(Student.new('11', age, name, parent_permission: parent_permission))
   end
 
-  # Create a teacher
+  # Create teacher
   def create_teacher
     print 'Age: '
     age = gets.chomp
 
-    print 'Name :'
+    print 'Name: '
     name = gets.chomp
 
     print 'Specialization: '
     specialization = gets.chomp
 
-    # Create a Teacher and push him to people array
+    # Create a student and push it to people array
     @people.push(Teacher.new(specialization, age, name))
   end
 
   # Create a person
   def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the nummber]: '
     case gets.chomp
     when '1'
       create_student
@@ -108,7 +107,7 @@ class App
     puts 'Person Created successfully'
   end
 
-  # Create a book.
+  # Create a Book
   def create_book
     print 'Title: '
     title = gets.chomp
@@ -116,19 +115,20 @@ class App
     print 'Author: '
     author = gets.chomp
 
-    # Create the book objec and add it to the books list
+    # Create the book object and add it to the books list
     @books.push(Book.new(title, author))
 
     # Success message
     puts 'Book Created successfully'
   end
 
-  # Create a rental.
+  # Create a rental
   def create_rental()
     # Select Book
     puts 'Select a book from the following list by number'
     list_all_books
     book_option = gets.chomp.to_i
+    puts
 
     # Select Person
     puts 'Select a person from the following list by number (not id)'
@@ -144,17 +144,17 @@ class App
     Rental.new(date, @books[book_option], @people[person_option])
 
     # Success Message
-    puts 'Rental created Successfully'
+    puts 'Rental created successfully'
   end
 
-  # List all rentals for a given person id.
+  # List all rentals for a given person id
   def rentals_of_person()
     # Select person
     print 'Id of person: '
     id = gets.chomp.to_i
     person_arr = @people.select { |person| person.id == id }
 
-    # Print the rentals for that person
+    # Print rentals for that person
     if person_arr.empty?
       puts 'No person matches the given ID!!'
     else
@@ -188,7 +188,7 @@ class App
   # Run the program
   def run
     puts 'Please choose an option by entering a number:'
-    display_cmd
+    display_command
     user_option_executer
   end
 end
